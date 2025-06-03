74f2dee3000b9021b1e7f1e8e58e19a19b91cebe
HyunJung <yhyunjung814@gmail.com>
2025-06-03 15:20:07 +0900

    design: 메인 페이지 디자인 수정

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -1,7 +1,9 @@
+import variables from "./variables.module.scss"
+
 export default function Home() {
   return (
-    <div className="flex items-center justify-items-center">
-     <main></main>
+    <div className="flex items-center justify-center w-full h-full">
+     <main className="font-bold" style={{ color: variables.primaryColor }}>This is the Dashboard page.</main>
     </div>
   );
 }
